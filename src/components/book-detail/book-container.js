5936ed6c98f76a3c2b66c089ca1b6104207bc8dd
Your Name <you@example.com>
2018-10-20 11:52:23 +0200

    MyBookAPP

---
+++ src/components/book-detail/book-container.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import BookInfo from './book-info';
+import BookReviewContainer from './book-review-container';
+
+let BookContainer = (props) => {
+    let bookInfo = {
+      image: props.book.volumeInfo.imageLinks.large,
+      title: props.book.volumeInfo.title,
+      author: props.book.volumeInfo.authors[0],
+      description: props.book.volumeInfo.description,
+      ratings: {
+        averageRating: props.book.volumeInfo.averageRating,
+        ratingCount: props.book.volumeInfo.ratingsCount,
+      }
+    }
+    return(
+        <div className="book-container">
+              <div className="book-img">
+                <img src={bookInfo.image} alt={bookInfo.title}/>
+              </div>
+              <BookInfo title={bookInfo.title} author={bookInfo.author}
+                description={bookInfo.description}/>
+
+              
+              <BookReviewContainer/> 
+              
+              
+            </div>
+    )
+}
+
+export default BookContainer;
